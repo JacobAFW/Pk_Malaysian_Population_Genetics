312330d6cd516492a076799b31190db5bda381e5
Jacob Westaway <jw1542@gadi-login-01.gadi.nci.org.au>
2022-05-09 16:38:00 +1000

    Updates

---
+++ scripts/05_Variant_filtering/Plot_quality_scores.R
@@ -0,0 +1,33 @@
+# load packages 
+library(tidyverse)
+library(knitr)
+library(gridExtra)
+
+# Plots
+
+variant_plots <- function(snp_filepath, indel_filepath){
+VCF <- read.csv(snp_filepath, header = T, na.strings=c("","NA"), sep = "\t") %>% 
+  add_column(Variant = "SNPs")  %>% 
+  rbind(
+    read.csv(indel_filepath, header = T, na.strings=c("","NA"), sep = "\t") %>% 
+      add_column(Variant = "Indels") 
+  )
+
+QD <- ggplot(VCF, aes(x=QD, fill=Variant)) + 
+  geom_density(alpha=.3) +
+  geom_vline(xintercept=20, size=0.7)
+
+FS <- ggplot(VCF, aes(x=FS, fill=Variant)) + 
+  geom_density(alpha=.3) +
+  geom_vline(xintercept=2, size=0.7)  
+
+MQ <- ggplot(VCF, aes(x=MQ, fill=Variant)) + 
+  geom_density(alpha=.3) +
+  geom_vline(xintercept=59, size=0.7) 
+
+grid.arrange(QD, FS, MQ)
+}
+
+variant_plots <- variant_plots("quality/GVCFall_SNPs.table", "quality/GVCFall_INDELs.table")
+
+ggsave("qualityVariant_quality_distribution.png", dpi = 600, variant_plots)
